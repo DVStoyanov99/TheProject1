f75456d4e991c71c7476468c972b33d9820a6993
DVStoyanov99 <57096368+DVStoyanov99@users.noreply.github.com>
2020-06-12 15:17:24 +0300

    test

---
+++ TheProject/Models/IEmployeerepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheProject.Models
+{
+    public interface _IEmployeeRepository
+    {
+        Employee GetEmployee(int Id);
+    }
+}
--- TheProject1/Controllers/HomeController.cs
+++ TheProject1/Controllers/HomeController.cs
@@ -3,13 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TheProject1.wwwroot.Controllers
+namespace TheProject1.Controllers
 {
     public class HomeController
     {
-        public string Index()
-        {
-            return "Hello from MVC";
-        }
     }
 }
